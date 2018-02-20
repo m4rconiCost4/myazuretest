c822ad8bcc8dc60bf2631a7f20c9072baa7fe26e
Marconi Costa De Souza Junior <marconi.junior@eldorado.org.br>
2018-02-20 10:42:06 -0300

    [progresso

--- src/Components/Azathoth.js
+++ src/Components/Azathoth.js
@@ -3,6 +3,27 @@ import { Link } from "react-router-dom";
 import aza from "../images/aza.jpg"
 import "./index.css";
 class Azathoth extends Component{
+    componentWillMount() {
+        console.log('Component WILL MOUNT!')
+     }
+     componentDidMount() {
+        console.log('Component DID MOUNT!')
+     }
+     componentWillReceiveProps(newProps) {    
+        console.log('Component WILL RECIEVE PROPS!')
+     }
+     shouldComponentUpdate(newProps, newState) {
+        return true;
+     }
+     componentWillUpdate(nextProps, nextState) {
+        console.log('Component WILL UPDATE!');
+     }
+     componentDidUpdate(prevProps, prevState) {
+        console.log('Component DID UPDATE!')
+     }
+     componentWillUnmount() {
+        console.log('Component WILL UNMOUNT!')
+     }
     render(){
         return(
         <div id= "body">
